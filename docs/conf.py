48365d6e00d8fb5234d2b9416a244c2907434db0
ponty <titi@pince.(none)>
2011-04-22 20:03:45 +0200

    build env

--- docs/conf.py
+++ docs/conf.py
@@ -1,23 +1,27 @@
-# -*- coding: utf-8 -*-
-
 from path import path
+from setuptools import find_packages
 import logging
 import sphinx
 import sys
 
-project='PyVirtualDisplay'
-author='ponty'
-copyright = '2011, ponty'
-PACKAGE = 'pyvirtualdisplay'
+def read_project_version(py=None, where='.', exclude=['bootstrap', 'pavement', 'doc', 'docs', 'test', 'tests', ]):
+    if not py:
+        py = path(where) / find_packages(where=where, exclude=exclude)[0]
+    py = path(py)
+    if py.isdir():
+        py = py / '__init__.py'
+    __version__ = None
+    for line in py.lines():
+        if '__version__' in line:
+            exec line
+            break
+    return __version__
 
-__version__ = None
-py = path('..') / PACKAGE / '__init__.py'
-for line in open(py).readlines():
-    if '__version__' in line:
-        exec line
-        break
-assert __version__    
-release = __version__
+release = read_project_version(where='..')
+
+project = 'PyVirtualDisplay'
+author = 'ponty'
+copyright = '2011, ponty'
 
 #logging.basicConfig(level=logging.DEBUG)
 
@@ -51,3 +55,8 @@ latex_documents = [
     ('index', '%s.tex' % project, u'%s Documentation' % project,
     author, 'manual'),
 ]
+
+
+# remove blank pages from pdf
+# http://groups.google.com/group/sphinx-dev/browse_thread/thread/92e19267d095412d/d60dcba483c6b13d
+latex_font_size = '10pt,oneside' 
--- pavement.py
+++ pavement.py
@@ -1,33 +1,42 @@
-# -*- Import: -*-
 from paver.easy import *
 from paver.setuputils import setup
 from setuptools import find_packages
 
 try:
     # Optional tasks, only needed for development
-    # -*- Optional import: -*-
     import paver.doctools
     import paver.virtual
     import paver.misctasks
+    from paved import *
+    from paved.dist import *
+    from paved.util import *
+    from paved.docs import *
+    from paved.pycheck import *
+    from sphinxcontrib import paverutils
     ALL_TASKS_LOADED = True
 except ImportError, e:
     info("some tasks could not not be imported.")
     debug(str(e))
     ALL_TASKS_LOADED = False
 
+def read_project_version(py=None, where='.', exclude=['bootstrap', 'pavement', 'doc', 'docs', 'test', 'tests', ]):
+    if not py:
+        py = path(where) / find_packages(where=where, exclude=exclude)[0]
+    py = path(py)
+    if py.isdir():
+        py = py / '__init__.py'
+    __version__ = None
+    for line in py.lines():
+        if '__version__' in line:
+            exec line
+            break
+    return __version__
+
 NAME = 'PyVirtualDisplay'
-PACKAGE = 'pyvirtualdisplay'
 URL = 'https://github.com/ponty/pyvirtualdisplay'
 DESCRIPTION = 'python wrapper for Xvfb and Xephyr'
+VERSION = read_project_version()
 
-__version__ = None
-py = path('.') / PACKAGE / '__init__.py'
-for line in open(py).readlines():
-    if '__version__' in line:
-        exec line
-        break
-assert __version__    
-version = __version__
 
 classifiers = [
     # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
@@ -44,20 +53,16 @@ install_requires = [
     'EasyProcess',
     ]
 
-entry_points = """
-    # -*- Entry points: -*-
-    """
-
 # compatible with distutils of python 2.3+ or later
 setup(
     name=NAME,
-    version=version,
+    version=VERSION,
     description=DESCRIPTION,
     long_description=open('README.rst', 'r').read(),
     classifiers=classifiers,
     keywords='Xvfb Xephyr X wrapper',
     author='ponty',
-    #author_email='zy@gmail.com',
+    #author_email='',
     url=URL,
     license='BSD',
     packages=find_packages(exclude=['bootstrap', 'pavement', ]),
@@ -65,89 +70,34 @@ setup(
     test_suite='nose.collector',
     zip_safe=False,
     install_requires=install_requires,
-    entry_points=entry_points,
     )
 
+
 options(
-    # -*- Paver options: -*-
-    minilib=Bunch(
-        extra_files=[
-            # -*- Minilib extra files: -*-
-            ]
-        ),
     sphinx=Bunch(
         docroot='docs',
         builddir="_build",
-        sourcedir=""
         ),
-    virtualenv=Bunch(
-        packages_to_install=[
-            # -*- Virtualenv packages to install: -*-
-            "nose",
-            "Sphinx>=0.6b1",
-            "pkginfo",
-            "virtualenv"],
-        dest_dir='./virtual-env/',
-        install_paver=True,
-        script_name='bootstrap.py',
-        paver_command_line=None
-        ),
+    pdf=Bunch(
+        builddir='_build',
+        builder='latex',
+    ),
     )
 
-options.setup.package_data = paver.setuputils.find_package_data(
-    PACKAGE, package=PACKAGE, only_in_packages=False)
-
 if ALL_TASKS_LOADED:
+    
+    options.paved.clean.patterns += ['*.pickle', '*.doctree', '*.gz' , 'nosetests.xml', 'sloccount.sc']
+    
+    options.paved.dist.manifest.include.remove('distribute_setup.py')
+    
+    
     @task
-    @needs('generate_setup', 'minilib', 'setuptools.command.sdist')
-    def sdist():
-        """Overrides sdist to make sure that our setup.py is generated."""
-
-@task
-def pychecker():
-    sh('pychecker --stdlib --only --limit 100 {package}/ '.format(package=PACKAGE))
-
-@task
-def findimports():
-    '''list external imports'''
-    sh('findimports {package} |grep -v ":"|grep -v {package}|sort|uniq'.format(package=PACKAGE))
-
-@task
-def pyflakes():
-    sh('pyflakes {package}'.format(package=PACKAGE))
-
-@task
-def nose():
-    sh('nosetests --with-xunit --verbose')
-
-@task
-def sloccount():
-    sh('sloccount --wide --details {package} tests > sloccount.sc'.format(package=PACKAGE))
-
-@task
-def clean():
-    root=path(__file__).dirname().abspath()
-    ls=[]
-    dls=[]
-    ls+=root.walkfiles('*.pyc')
-    ls+=root.walkfiles('*.html')
-    ls+=root.walkfiles('*.zip')
-    ls+=root.walkfiles('*.css')
-    ls+=root.walkfiles('*.png')
-    ls+=root.walkfiles('*.doctree')
-    ls+=root.walkfiles('*.pickle')
-
-    dls+=[root / 'dist']
-    dls+=root.listdir('*.egg-info')
-
-    for x in ls:
-        x.remove()
-    for x in dls:
-        x.rmtree()
-
-@task
-@needs('sloccount', 'html', 'sdist', 'nose')
-def hudson():
-    pass
-
+    @needs('sloccount', 'html', 'sdist', 'nose')
+    def hudson():
+        pass
+    
+    @task
+    @needs('sphinxcontrib.paverutils.html')
+    def html():
+        pass
 
