338476fd658f46081afb366e52034fc13c34cec3
ponty <ponty@home>
2012-03-10 19:15:29 +0100

    tox+setup

--- pavement.py
+++ pavement.py
@@ -2,71 +2,20 @@ from paver.easy import *
 from paver.setuputils import setup
 from setuptools import find_packages
 
-try:
-    # Optional tasks, only needed for development
-    import paver.doctools
-    import paver.virtual
-    import paver.misctasks
-    from paved import *
-    from paved.dist import *
-    from paved.util import *
-    from paved.docs import *
-    from paved.pycheck import *
-    from paved.pkg import *
-    from sphinxcontrib import paverutils
-    ALL_TASKS_LOADED = True
-except ImportError, e:
-    info("some tasks could not not be imported.")
-    debug(str(e))
-    ALL_TASKS_LOADED = False
+import paver.doctools
+import paver.virtual
+import paver.misctasks
+from paved import *
+from paved.dist import *
+from paved.util import *
+from paved.docs import *
+from paved.pycheck import *
+from paved.pkg import *
+from sphinxcontrib import paverutils
 
-def read_project_version(py=None, where='.', exclude=['bootstrap', 'pavement', 'doc', 'docs', 'test', 'tests', ]):
-    if not py:
-        py = path(where) / find_packages(where=where, exclude=exclude)[0]
-    py = path(py)
-    if py.isdir():
-        py = py / '__init__.py'
-    __version__ = None
-    for line in py.lines():
-        if '__version__' in line:
-            exec line
-            break
-    return __version__
-
-NAME = 'PyVirtualDisplay'
-URL = 'https://github.com/ponty/PyVirtualDisplay'
-DESCRIPTION = 'python wrapper for Xvfb, Xephyr and Xvnc'
-VERSION = read_project_version()
-
-
-classifiers = [
-    # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-    "License :: OSI Approved :: BSD License",
-    "Natural Language :: English",
-    "Operating System :: OS Independent",
-    "Programming Language :: Python",
-    ]
-
-install_requires = open("requirements.txt").read().split('\n')
-
-# compatible with distutils of python 2.3+ or later
-setup(
-    name=NAME,
-    version=VERSION,
-    description=DESCRIPTION,
-    long_description=open('README.rst', 'r').read(),
-    classifiers=classifiers,
-    keywords='Xvfb Xephyr X wrapper',
-    author='ponty',
-    #author_email='',
-    url=URL,
-    license='BSD',
-    packages=find_packages(exclude=['bootstrap', 'pavement', ]),
-    include_package_data=True,
-    test_suite='nose.collector',
-    zip_safe=False,
-    install_requires=install_requires,
-    )
+# get info from setup.py
+setup_py=''.join([x for x in path('setup.py').lines() if 'setuptools' not in x])
+exec(setup_py)
 
 
 options(
@@ -80,46 +29,62 @@ options(
     ),
     )
 
-if ALL_TASKS_LOADED:
-    
-    options.paved.clean.patterns += ['*.pickle',
-                                     '*.doctree',
-                                     '*.gz' ,
-                                     'nosetests.xml',
-                                     'sloccount.sc',
-                                     '*.pdf', '*.tex',
-                                     '*.png',
-                                     ]
-    
-    options.paved.dist.manifest.include.remove('distribute_setup.py')
-    options.paved.dist.manifest.include.add('requirements.txt')
-    
-    
-    @task
-    @needs(
-           'clean',
-           'sloccount',
-           'html',
-           'pdf',
-           'sdist',
-           'nose',
-           )
-    def alltest():
-        'all tasks to check'
-        pass
-    
-    @task
-    @needs('sphinxcontrib.paverutils.html')
-    def html():
-        pass
+options.paved.clean.rmdirs +=   ['.tox',
+                                 'dist',
+                                 'build' ,
+                                 ]
+options.paved.clean.patterns += ['*.pickle',
+                                 '*.doctree',
+                                 '*.gz' ,
+                                 'nosetests.xml',
+                                 'sloccount.sc',
+                                 '*.pdf', '*.tex',
+                                 '*.png',
+                                 '*.zip',   
+                                 'distribute_setup.py',
+                                 ]
 
-    @task
-    @needs('sphinxcontrib.paverutils.pdf')
-    def pdf():
-        fpdf = list(path('docs/_build/latex').walkfiles('*.pdf'))[0]
-        d = path('docs/_build/html')
-        d.makedirs()
-        fpdf.copy(d)
+options.paved.dist.manifest.include.remove('distribute_setup.py')
+options.paved.dist.manifest.include.remove('paver-minilib.zip')
+options.paved.dist.manifest.include.add('requirements.txt')
 
 
+@task
+@needs(
+#           'clean',
+       'sloccount',
+       'html',
+       'pdf',
+       'sdist',
+       'nose',   'tox',
+       )
+def alltest():
+    'all tasks to check'
+    pass
+
+@task
+@needs('sphinxcontrib.paverutils.html')
+def html():
+    pass
+
+@task
+@needs('sphinxcontrib.paverutils.pdf')
+def pdf():
+    fpdf = list(path('docs/_build/latex').walkfiles('*.pdf'))[0]
+    d = path('docs/_build/html')
+    d.makedirs()
+    fpdf.copy(d)
+
+@task
+def tox():
+    '''Run tox.'''
+    sh('tox')
+
+    
+@task
+@needs('manifest', 'setuptools.command.sdist')
+def sdist():
+    """Overrides sdist to make sure that our MANIFEST.in is generated.
+    """
+    pass
         
