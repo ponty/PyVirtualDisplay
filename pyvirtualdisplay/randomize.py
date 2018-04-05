e36f2c7ff7c731201243ffaf182d5497c0b67738
Thomas van Doornmalen <thomas.vandoornmalen@coosto.com>
2018-04-05 15:15:58 +0200

    Improve docs for Randomizer

--- pyvirtualdisplay/randomize.py
+++ pyvirtualdisplay/randomize.py
@@ -1,6 +1,11 @@
 import random
 
 class Randomizer():
+    '''
+    Class to generate random display numbers between a minimum and maximum
+    value. Note that the maximum value can be exceeded if every value between
+    the generated and maximum value is already in use.
+    '''
     def __init__(self, min_display_nr=1000, max_display_nr=1100):
         self.min = min_display_nr
         self.delta = max_display_nr - min_display_nr
@@ -8,8 +13,10 @@ class Randomizer():
         random.seed()
 
     def generate(self):
-        """
-        Get a random display number
-        """
+        '''
+        Generate a random display number
+
+        :rtype: int
+        '''
 
         return self.min + random.randint(0, self.delta)
