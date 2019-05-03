29e74baf25d012118c42645e8763ea61415f2bd6
Rob Chiocchio <RobethX@users.noreply.github.com>
2019-05-03 13:11:21 -0400

    Added the -rfbauth flag to the Xvnc command
    Default is set to '', which is the default listed in Xvnc's documentation

--- pyvirtualdisplay/xvnc.py
+++ pyvirtualdisplay/xvnc.py
@@ -13,7 +13,7 @@ class XvncDisplay(AbstractDisplay):
     '''
     Xvnc wrapper
     '''
-    def __init__(self, size=(1024, 768), color_depth=24, bgcolor='black', rfbport=5900):
+    def __init__(self, size=(1024, 768), color_depth=24, bgcolor='black', rfbport=5900, rfbauth=''):
         '''
         :param bgcolor: 'black' or 'white'
         :param rfbport: Specifies the TCP port on which Xvnc listens for connections from viewers (the protocol used in VNC is called RFB - "remote framebuffer"). The default is 5900 plus the display number.
@@ -25,6 +25,7 @@ class XvncDisplay(AbstractDisplay):
         self.bgcolor = bgcolor
         self.display = None
         self.rfbport = rfbport
+        self.rfbauth = rfbauth
         AbstractDisplay.__init__(self)
 
     @classmethod
@@ -38,6 +39,7 @@ class XvncDisplay(AbstractDisplay):
                '-depth', str(self.color_depth),
                '-geometry', '%dx%d' % (self.size[0], self.size[1]),
                '-rfbport', str(self.rfbport),
+               '-rfbauth', str(self.rfbauth),
                self.new_display_var,
                ]
         return cmd
