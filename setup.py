bcefb249394f5e485d259ee9fc987d04e0cc3f39
ponty <ponty@home>
2020-01-08 13:49:47 +0100

    remove 2to3

--- setup.py
+++ setup.py
@@ -22,10 +22,10 @@ __version__ = None
 exec(open(os.path.join(NAME, 'about.py')).read())
 VERSION = __version__
 
-extra = {}
-if sys.version_info >= (3,):
-    extra['use_2to3'] = True
-    extra['use_2to3_exclude_fixers'] = ['lib2to3.fixes.fix_import']
+# extra = {}
+# if sys.version_info >= (3,):
+#     extra['use_2to3'] = True
+#     extra['use_2to3_exclude_fixers'] = ['lib2to3.fixes.fix_import']
 
 classifiers = [
     # Get more strings from
@@ -61,5 +61,5 @@ setup(
     #     test_suite='nose.collector',
     #     zip_safe=False,
     install_requires=install_requires,
-    **extra
+    # **extra
 )
